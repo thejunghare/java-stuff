663bc90bcecc49e1c0149d97a59eb1430cf083be
thejunghare <prasadjunghare2@gmail.com>
2025-09-13 14:07:17 +0530

    update code

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,19 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	int age = 24;
-
-	// method
-	public int sum() {
-		return this.age + 10;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-		DemoApplication obj = new DemoApplication();
-		System.out.println(obj.sum());
 	}
 
 }
---
+++ demo/src/main/java/com/example/demo/model/Task.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+public class Task {
+    private int id;
+    private String name;
+
+    public Task() {
+
+    }
+
+    public Task(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
